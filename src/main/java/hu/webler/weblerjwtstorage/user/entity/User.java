424158fcb715b34d308cec2739f28fff138b342e
Csaba Vadász <csabavadasz79@gmail.com>
2024-03-16 11:29:45 +0100

    create User entity

---
+++ src/main/java/hu/webler/weblerjwtstorage/user/entity/User.java
@@ -0,0 +1,25 @@
+package hu.webler.weblerjwtstorage.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "webler_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
