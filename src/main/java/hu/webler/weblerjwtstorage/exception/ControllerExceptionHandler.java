d589e91afb17bd4693909d55ab8eb47026a1eb94
Csaba Vadász <csabavadasz79@gmail.com>
2024-03-16 14:20:33 +0100

    common exception handling (ControllerAdvice & enum)

---
+++ src/main/java/hu/webler/weblerjwtstorage/exception/ControllerExceptionHandler.java
@@ -0,0 +1,25 @@
+package hu.webler.weblerjwtstorage.exception;
+
+import hu.webler.weblerjwtstorage.user.value.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+import static hu.webler.weblerjwtstorage.user.value.ErrorCode.ERROR_CODE_001;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(value = {UsernameNotFoundException.class})
+    public ResponseEntity<Object> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        return new ResponseEntity<>(responseBodyWithMessage(ERROR_CODE_001, ex.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    private String responseBodyWithMessage(ErrorCode code, String message) {
+        return Map.of(code, message).toString();
+    }
+}
---
+++ src/main/java/hu/webler/weblerjwtstorage/user/value/ErrorCode.java
@@ -0,0 +1,6 @@
+package hu.webler.weblerjwtstorage.user.value;
+
+public enum ErrorCode {
+
+    ERROR_CODE_001
+}
